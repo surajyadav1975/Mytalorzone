7f572e70ccb5e808008dda388c4c11ef9979fa66
surajyadav1975fk <ysuraj1191@gmail.com>
2024-12-22 23:45:21 +0530

    deploy

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -29,6 +29,7 @@ const Login = () => {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify(formData),
+            credentials: 'include',
           });
     
           const result = await response.json();
