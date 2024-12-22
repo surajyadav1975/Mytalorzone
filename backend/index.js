f3e8ea640fde2065d51600bc87172f153c94a2d3
surajyadav1975fk <ysuraj1191@gmail.com>
2024-12-22 23:38:59 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "https://mytalorzone-g2mr-67f6f9ohp-suraj-yadavs-projects-35b9b057.vercel.app"
-    ],
+    origin:'https://mytalorzone-iota.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
