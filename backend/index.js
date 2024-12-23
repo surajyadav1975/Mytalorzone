46b6fe0e9b756c5f5030730bfd7969469844077f
Suraj Yadav <ysuraj1191@gmail.com>
2024-12-23 11:22:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin:'http://localhost:5174',
+    origin:'https://mytalorzone-n6ak.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
